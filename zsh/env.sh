83a047406c426afc64e4d093d23fb24fb7b022f1
Mirko Akov <mupkoo@gmail.com>
2015-12-13 23:48:20 +0200

    Add ZSH config files

---
+++ zsh/env.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+export EDITOR='subl -w'
+export BUNDLER_EDITOR='subl -w'
+
+function start {
+    if [ -f "Procdev" ] ; then
+        echo "Clean tmp folder?"
+        rm -rf tmp/*
+        echo "Clean log folder?"
+        rm -rf log/*
+        echo "Starting Foreman project"
+        foreman start -f Procdev
+    elif [ -f "package.json" ] ; then
+        echo "Clean tmp folder?"
+        rm -rf tmp/*
+        echo "Starting node project"
+        npm start
+    else
+        echo "The folder does not have Procdev or package.json files"
+    fi
+}
